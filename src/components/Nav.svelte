4ec870e7ce83c3909c61c757f750ce441d20077e
jeffwscott <jeffscott@outlook.com>
2020-08-31 17:06:39 -0400

    nav logo center on mobile

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -9,25 +9,46 @@
 </script>
 
 <style>
-	nav {
-		z-index: 50;
+	nav{
+		width: 100%;
 		border-bottom: 1px solid var(--font-grey);
-		padding: 2rem 5rem;
-		align-items: center;
-		justify-content: flex-start;
 	}
-	nav> span:first-child{
+	div {
+		z-index: 50;
+		padding: 2rem;
+		align-items: center;
+		justify-content: center;
+		margin: 0 auto;
+		max-width: 1920px;
+    	
+	}
+	div > span:first-child{
 		width: 35px;
 	}
-	nav > span:last-child{
+	div > span:last-child{
 		width: 125px;
 		margin-left: 15px;
 	}
 
+	@media (min-width: 450px) {
+		div{
+			justify-content: flex-start;
+		}
+	}
+
+	@media (min-width: 850px) {
+		div{
+			padding: 2rem 4rem;
+		}
+	}
+
 </style>
 
-<nav class="flex-row">
-	<span >{@html lamdenLogo}</span>
-	<span>{@html lamdenWords}</span>
+<nav>
+	<div class="flex-row">
+		<span >{@html lamdenLogo}</span>
+		<span>{@html lamdenWords}</span>
+	</div>
+
 </nav>
 
